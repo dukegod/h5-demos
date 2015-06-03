3e2915732fd26553562b8ba25ba930c188725399
duke <841530656@qq.com>
2015-06-03 18:50:46 +0800

    less

--- js/duke-touch.js
+++ js/duke-touch.js
@@ -2,17 +2,17 @@
 //可以在pc上和在手机上转换
 //
 var dkTouchEvents = {
-        touchstart: "touchstart",
-        touchmove: "touchmove",
-        touchend: "touchend",
-        initTouchEvents: function () {
-            if (isPC()) {
-                this.touchstart = "mousedown";
-                this.touchmove = "mousemove";
-                this.touchend = "mouseup";
-                console.log(this);
-            }
-        }
+	touchstart: "touchstart",
+	touchmove: "touchmove",
+	touchend: "touchend",
+	initTouchEvents: function () {
+	    if (isPC()) {
+	        this.touchstart = "mousedown";
+	        this.touchmove = "mousemove";
+	        this.touchend = "mouseup";
+	        console.log(this);
+	    }
+	}
 };
 
 function isPC(){
@@ -39,22 +39,22 @@ function IsPC(){
 //判断是否是移动或是web
  var browser={  
     versions:function(){   
-           var u = navigator.userAgent, 
-           app = navigator.appVersion;
-           return {
-           		//移动终端浏览器版本信息
-                trident: u.indexOf('Trident') > -1, //IE内核  
-                presto: u.indexOf('Presto') > -1, //opera内核  
-                webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核  
-                gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核  
-                mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端  
-                ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端  
-                android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器  
-                iPhone: u.indexOf('iPhone') > -1 , //是否为iPhone或者QQHD浏览器  
-                iPad: u.indexOf('iPad') > -1, //是否iPad    
-                webApp: u.indexOf('Safari') == -1 //是否web应该程序，没有头部与底部  
-            };  
-         }(),  
+       var u = navigator.userAgent, 
+       app = navigator.appVersion;
+       return {
+       		//移动终端浏览器版本信息
+            trident: u.indexOf('Trident') > -1, //IE内核  
+            presto: u.indexOf('Presto') > -1, //opera内核  
+            webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核  
+            gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核  
+            mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端  
+            ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端  
+            android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器  
+            iPhone: u.indexOf('iPhone') > -1 , //是否为iPhone或者QQHD浏览器  
+            iPad: u.indexOf('iPad') > -1, //是否iPad    
+            webApp: u.indexOf('Safari') == -1 //是否web应该程序，没有头部与底部  
+        };  
+     }(),  
     language:(navigator.browserLanguage || navigator.language).toLowerCase()  
 }   
   
@@ -136,11 +136,11 @@ var slideFunc= function(){
         document.getElementById("version").innerHTML = navigator.appVersion;
         try {  
             document.createEvent("TouchEvent");  
-            alert("支持触摸事件 ！");  
+//          alert("支持触摸事件 ！");  
              bindEvent(); //绑定事件  
         }  
         catch (e) {  
-            alert("不支持触摸事件 ！" + e.message);  
+//          alert("不支持触摸事件 ！" + e.message);  
         }  
     } 
     isTouchDevice();
