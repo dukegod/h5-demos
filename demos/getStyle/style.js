57e4188f01afc933ac119a60079c6ffe4b961031
liuhui <dukehui1991@gmail.com>
2016-03-21 00:03:19 +0800

    总结比较获取样式以及伪类元素的方法
    pseudo element

--- demos/getStyle/style.js
+++ demos/getStyle/style.js
@@ -4,13 +4,14 @@
  */
 'use strict';
 // ele 为你输入的style，e为具体的样式
-function getStyle(ele, e) {
-	var style = null;
-	// 判断浏览器是不是支持
-	if (window.getComputedStyle) {
-		style = window.getComputedStyle(ele, null).getPropertyValue(e);
-	} else {
-		style = ele.currentStyle.getPropertyCSSValue(e);
-	}
-	return style;
-}	
+function getStyle(ele, pseudo, e) {
+  var ps = pseudo || null
+  var style = null;
+  // 判断浏览器是不是支持
+  if (window.getComputedStyle) {
+    style = window.getComputedStyle(ele, ps).getPropertyValue(e);
+  } else {
+    style = ele.currentStyle.getPropertyCSSValue(e);
+  }
+  return style;
+}
