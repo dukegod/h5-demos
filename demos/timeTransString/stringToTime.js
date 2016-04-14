08687e91c719746e7dcdef33b23c2743cc005ae8
liuhui <dukehui1991@gmail.com>
2016-04-14 10:48:52 +0800

    时间戳

--- demos/timeTransString/stringToTime.js
+++ demos/timeTransString/stringToTime.js
@@ -6,10 +6,14 @@ function timeTrans(e) {
   var date = new Date(e);
   var Y = date.getFullYear() + '-';
   var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
-  var D = (date.getDate() + 1 < 10 ? '0' + (date.getDate() + 1) : date.getDate() + 1);
+  var D = date.getDate() + 1 < 10 ? '0' + date.getDate() : date.getDate();
   var da = Y + M + D;
+  var D = date.getDate() + ' ';
+	var h = date.getHours() + ':';
+	var m = date.getMinutes() + ':';
+	var s = date.getSeconds(); 
   console.log(da);
   return da;
 }
 
-timeTrans();
+timeTrans(1451606400000);
