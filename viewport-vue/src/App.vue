be397f3ad8615613b8a617657f5098ef23988dce
dukegod <dukehui1991@gmail.com>
2018-10-11 12:09:41 +0800

    完善方案

--- viewport-vue/src/App.vue
+++ viewport-vue/src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="wrapper">
+    <div class="rtc-300">
+      我是一个400*400的正方形
+    </div>
     <div class="box1"></div>
     <div class="box2"></div>
     <div class="box3"></div>
     <div class="box4"></div>
     <div class="box5"></div>
+
+    <p> 一下为引入的第三方插件样式：</p>
     <mt-button type="default">default</mt-button>
     <mt-button type="primary">primary</mt-button>
     <mt-button type="danger">danger</mt-button>
@@ -29,39 +34,50 @@
 </script>
 
 <style>
-  * {
-    margin: 0;
-    padding: 0;
-  }
-  </style>
+* {
+  margin: 0;
+  padding: 0;
+  font-size: 30px;
+}
+</style>
 
-  <style lang="less" scoped>
-  .wrapper div {
-    height: 1rem;
-  }
-  .box1 {
-    /* 750 * 20% */
-    width: 150px;
-    background-color: coral;
-  }
-  .box2 {
-    /* 750 * 40% */
-    width: 300px;
-    background-color: skyblue;
-  }
-  .box3 {
-    /* 750 * 60% */
-    width: 450px;
-    background-color: palegreen;
-  }
-  .box4 {
-    /* 750 * 80% */
-    width: 600px;
-    background-color: wheat;
-  }
-  .box5 {
-    /* 750 * 100% */
-    width: 750px;
-    background-color: darkred;
-  }
+<style lang="less" scoped>
+
+@baseFont: 2;
+
+.wrapper div {
+  height: 1rem;
+}
+.rtc-300 {
+  width: 400px/@baseFont;
+  height: 400px/@baseFont !important;
+  background-color: black;
+  color: white;
+  font-size: 30px/@baseFont;
+}
+.box1 {
+  /* 750 * 20% */
+  width: 150px/@baseFont;
+  background-color: coral;
+}
+.box2 {
+  /* 750 * 40% */
+  width: 300px/@baseFont;
+  background-color: skyblue;
+}
+.box3 {
+  /* 750 * 60% */
+  width: 450px/@baseFont;
+  background-color: palegreen;
+}
+.box4 {
+  /* 750 * 80% */
+  width: 600px/@baseFont;
+  background-color: wheat;
+}
+.box5 {
+  /* 750 * 100% */
+  width: 750px/@baseFont;
+  background-color: darkred;
+}
 </style>
